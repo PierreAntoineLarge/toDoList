00102bc6ff61e45382b82feea484cf02ec31c7e1
pa large <pierreantoine.large@gmail.com>
2023-04-14 12:01:53 +0200

    add item to the to do list

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-s
+/**
+ * 
+ * @param {{title:string, body:string}} item 
+ * @return {HTMLElement}
+ */
+function createItem(item){
+    const lister = document.createElement('li')
+    const ensemble = document.querySelector('ul')
+    console.log(ensemble)
+    
+    const ici = lister.innerText = item.title
+    console.log(ici)
+    lister.classList.add("todo")
+    lister.classList.add("list-group-item")
+    lister.classList.add("d-flex")
+    lister.classList.add("align-item-center")
+    ensemble.appendChild(lister)
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    lister.insertBefore(checkbox, ici)
+
+    return lister
+}
+
+async function main(){
+
+    const r = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5',{
+        headers: {
+            Accept: 'application/json'
+        }
+    })
+    console.log(r)
+
+    const list = document.querySelector('.list-group')
+
+    const items = await r.json()
+    console.log(list)
+    for (let item of items){
+        list.append(createItem(item))
+        
+    }
+
+}
+
+main()
