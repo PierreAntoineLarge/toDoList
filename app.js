e67ebee9a2eaf78ef0e7bd6674c244aecbf43274
pa large <pierreantoine.large@gmail.com>
2023-04-18 13:06:52 +0200

    formData

--- app.js
+++ app.js
@@ -165,7 +165,13 @@ main()
 
 
 
-
+export function createElement(tagName, attributes = {}){
+    const element = document.createElement(tagName)
+    for (const [attribute, value] of Object.entries(attribute))
+        if {value !== false && value !== null} {
+            element.setAttribute(attribute, value)
+        }
+}
 
 
 
